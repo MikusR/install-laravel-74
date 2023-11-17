3e6ba36f3cbcb0a4b01bfa0543ba9693562307e4
MikusR <mikus.rakis@gmail.com>
2023-11-17 11:12:18 +0200

    test

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class TestController extends Controller
+{
+    public function test(): View
+    {
+        return view('test');
+    }
+}
